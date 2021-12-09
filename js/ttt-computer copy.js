40435674afc5fc0cf2d3d8fe828a69c22bac17cc
Randeep Singh <71288034+RaSingh02@users.noreply.github.com>
2021-12-08 23:14:15 -0500

    Added gradients to main page text, updated text animation, modified computerPlay function in TTT

---
+++ js/ttt-computer copy.js
@@ -0,0 +1,115 @@
+const statusDisplay = document.querySelector('.game--status');
+
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+statusDisplay.innerHTML = currentPlayerTurn();
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
+}
+
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
+
+    if (roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
+        return;
+    }
+
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
+    }
+}
+
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+}
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
+
+
+function sleep(milliseconds) {  
+    return new Promise(resolve => setTimeout(resolve, milliseconds));  
+}  
+
+async function compPlay() { //function to fill a random square
+  let emptyCells = []; //creates array for empty cells
+  const allCells = document.querySelectorAll('.cell'); //sets up array of all cells
+  for (const aCell of allCells) { //loops through array to find empties
+    if (aCell.innerHTML == "") { //if a cell is empty . . .
+      emptyCells.push(aCell);    //add it to the array of empties
+    }
+  }
+
+  let rand = Math.floor(Math.random() * emptyCells.length); 
+
+  if (emptyCells.length > 0) { //as long as there are empty cells . . .
+    if (emptyCells[rand].innerHTML == "") { //and if a random cell is empty  . . .
+      await sleep(2000);
+      emptyCells[rand].innerHTML = "O"; //fill that cell with with a "O"
+      currentPlayer = "X";
+      statusDisplay.innerHTML = currentPlayerTurn();
+      handleResultValidation();
+    }
+  } else if (emptyCells == 0) { 
+    handleResultValidation(); 
+    return; 
+  }
+}
