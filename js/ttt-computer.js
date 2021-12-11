ec82ed64044aa9940bd08a126deb45a63087d085
Randeep Singh <71288034+RaSingh02@users.noreply.github.com>
2021-12-10 22:20:08 -0500

    Renamed File

--- js/ttt-computer.js
+++ js/ttt-computer.js
@@ -59,83 +59,18 @@ function handleResultValidation() {
         gameActive = false;
         return;
     }
-
-    handlePlayerChange();
 }
 
-function randomInt(max) {
-    return Math.floor(Math.random() * max);
-}
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
 
-if (currentPlayer == 'X') {
-    function handleCellClick(clickedCellEvent) {
-        const clickedCell = clickedCellEvent.target;
-        const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
-
-        if (gameState[clickedCellIndex] !== "" || !gameActive) {
-            return;
-        }
-
-        handleCellPlayed(clickedCell, clickedCellIndex);
-        handleResultValidation();
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
     }
-} else if (currentPlayer == "O") {
-    function handleCellClickComputer() {
 
-        let counter = 0;
-        const isEmpty = 0;
-
-        while (counter != 1) {
-            let rInt =  randomInt(9)
-            if (gameState[rInt] == "") {
-                isEmpty = rInt;
-                counter = 1;
-            }
-        }
-
-        switch (isEmpty) {
-            case (isEmpty = 0):
-                gameState[isEmpty] = currentPlayer;
-                document.getElementById("c0").innerHTML = currentPlayer;
-                break;
-            case (isEmpty = 1):
-                gameState[isEmpty] = currentPlayer;
-                document.getElementById("c1").innerHTML = currentPlayer;
-                break;
-            case (isEmpty = 2):
-                gameState[isEmpty] = currentPlayer;
-                document.getElementById("c2").innerHTML = currentPlayer;
-                break;
-            case (isEmpty = 3):
-                gameState[isEmpty] = currentPlayer;
-                document.getElementById("c3").innerHTML = currentPlayer;
-                break;
-            case (isEmpty = 4):
-                gameState[isEmpty] = currentPlayer;
-                document.getElementById("c4").innerHTML = currentPlayer;
-                break;
-            case (isEmpty = 5):
-                gameState[isEmpty] = currentPlayer;
-                document.getElementById("c5").innerHTML = currentPlayer;
-                break;
-            case (isEmpty = 6):
-                gameState[isEmpty] = currentPlayer;
-                document.getElementById("c6").innerHTML = currentPlayer;
-                break;
-            case (isEmpty = 7):
-                gameState[isEmpty] = currentPlayer;
-                document.getElementById("c7").innerHTML = currentPlayer;
-                break;
-            case (isEmpty = 8):
-                gameState[isEmpty] = currentPlayer;
-                document.getElementById("c8").innerHTML = currentPlayer;
-                break;
-            default:
-                break;
-        }
-
-        handleResultValidation();
-    }
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
 }
 
 function handleRestartGame() {
@@ -149,62 +84,51 @@ function handleRestartGame() {
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
 document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
 
-
-//Note that you WILL need to change the fill character on the alternate clicks.
-//You will also need to verify win conditions are met and output properly
-//This runs when a button is clicked, but you should integrate it better
-function compPlay() { //function to fill a random square
-  let emptyCells = []; //creates array for empty cells
-  const allCells = document.querySelectorAll('.cell'); //sets up array of all cells
-  for (const aCell of allCells) { //loops through array to find empties
-    if (aCell.innerHTML == "") { //if a cell is empty . . .
-      emptyCells.push(aCell);    //add it to the array of empties
+function compPlay() { 
+    let emptyCells = []; 
+    const allCells = document.querySelectorAll('.cell'); 
+    for (const aCell of allCells) { 
+      if (aCell.innerHTML == "") { 
+        emptyCells.push(aCell);    
+      }
     }
-  }
+  
+    let rand = Math.floor(Math.random() * emptyCells.length);
+  
+    if (emptyCells.length > 0) {
+      if (emptyCells[rand].innerHTML == "") {
+        randCellIndex = parseInt(emptyCells[rand].getAttribute('data-cell-index'));
+        gameState[randCellIndex] = "O"; 
+        emptyCells[rand].innerHTML = "O";
 
-  let rand = Math.floor(Math.random() * emptyCells.length); //generates a random number between 0 and the number of empty cells
+        let roundWon = false;
+        for (let i = 0; i <= 7; i++) {
+            const winCondition = winningConditions[i];
+            let a = gameState[winCondition[0]];
+            let b = gameState[winCondition[1]];
+            let c = gameState[winCondition[2]];
+            if (a === '' || b === '' || c === '') {
+                continue;
+            }
+            if (a === b && b === c) {
+                roundWon = true;
+                break
+            }
+        }
 
-  if (emptyCells.length > 0) { //as long as there are empty cells . . .
-    if (emptyCells[rand].innerHTML == "") { //and if a random cell is empty  . . .
-      emptyCells[rand].innerHTML = "O"; //fill that cell with with a "O"
+        if (roundWon) { 
+            const computerWin = () => `Player O has won!`;
+            statusDisplay.innerHTML = computerWin();
+            gameActive = false;
+            return;
+        }
+
+        let roundDraw = !gameState.includes("");
+        if (roundDraw) {
+            statusDisplay.innerHTML = drawMessage();
+            gameActive = false;
+            return;
+        }
+      }
     }
-  } else { //if no empty cells remain . . .
-    console.log("Game Over!"); //log game over . . .
-    return; //and end play
-  }
-
-  //logs, just FYI
-  console.log("Rand #: " + rand);
-  console.log("# of Empties: " + emptyCells.length);
 }
-
-
-// async function compPlay() { //function to fill a random square
-//     let emptyCells = []; //creates array for empty cells
-//     const allCells = document.querySelectorAll('.cell'); //sets up array of all cells
-//     for (const aCell of allCells) { //loops through array to find empties
-//       if (aCell.innerHTML == "") { //if a cell is empty . . .
-//         emptyCells.push(aCell);    //add it to the array of empties
-//       }
-//     }
-  
-//     let rand = Math.floor(Math.random() * emptyCells.length); 
-  
-//     if (emptyCells.length > 0) { //as long as there are empty cells . . .
-//       if (emptyCells[rand].innerHTML == "") { //and if a random cell is empty  . . .
-//         statusDisplay.innerHTML = currentPlayerTurn();
-//         await sleep(2000);
-//         emptyCells[rand].innerHTML = "O"; //fill that cell with with a "O"
-//         currentPlayer = "O";
-//         gameState[rand] = currentPlayer;
-//         handleResultValidation();
-//         currentPlayer = "X";
-//         statusDisplay.innerHTML = currentPlayerTurn();
-//         emptyCells.length = 0;
-  
-//       }
-//     } else if (emptyCells == 0) { 
-//       handleResultValidation(); 
-//       return; 
-//     }
-// }
